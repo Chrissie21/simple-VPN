86daa4ad11ec48dffa4668d20e0b25e6ed2198cf
Chris <chrisbalele21@gmail.com>
2025-06-07 06:07:32 +0300

    AES-GCM encryptor

---
+++ crypto/crypto.go
@@ -0,0 +1,56 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"io"
+)
+
+type VPNCrypto struct {
+	gcm cipher.AEAD
+}
+
+// new AES-GCM
+func NewVPNCrypto(key []byte) (*VPNCrypto, error) {
+	if len(key) != 32 {
+		return nil, errors.New("key must be 32 bytes")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	return &VPNCrypto{gcm: gcm}, nil
+}
+
+// Encrypt data using AES-GCM
+func (v *VPNCrypto) Encrypt(plain []byte) ([]byte, error) {
+	nonce := make([]byte, v.gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext := v.gcm.Seal(nil, nonce, plain, nil)
+	return append(nonce, ciphertext...), nil
+}
+
+// Decrypt data using AES-GCM
+func (v *VPNCrypto) Decrypt(ciphertext []byte) ([]byte, error) {
+	ns := v.gcm.NonceSize()
+	if len(ciphertext) < ns {
+		return nil, errors.New("ciphertext too short")
+	}
+
+	nonce := ciphertext[:ns]
+	enc := ciphertext[ns:]
+
+	return v.gcm.Open(nil, nonce, enc, nil)
+}
